0a1399c19ef3787cb6b95ecd38420635cf31ea20
chisomaga mikel <chisomagamikel@gmail.com>
2024-09-02 22:21:33 +0100

    java

---
+++ main.js
@@ -0,0 +1,64 @@
+// console.log("Hello world");
+// console.log(5 + 5);
+// alert("what is your name");
+
+// var VisitorName = prompt("what's your name :");
+// alert("my name is" + VisitorName);
+//  console.log(VisitorName");
+
+// console.log(20 > 100);
+
+// var mikel = 20;
+// var matthew=100;
+
+// console.log(mikel+ matthew);
+
+// console.log(5 <= 5);
+// console.log(5<5);
+
+// function greet(name){ return `Hello ${name.toUpperCase()}`} 
+let space ='   '
+let firstName = "Asabeneh";
+let country = "Finland";
+let city = "Helsinki";
+let age = 250;
+let isMarried = true;
+let lastName = "Yetayeh";
+let job = "teacher";
+let language = "JavaScript";
+let fullName = firstName + space + lastName;
+let personInfoOne = fullName + ".I am + age " + "I live in " + country;
+console.log(  personInfoOne);
+
+let personInfoOTwo = `I am ${fullName}.I am ${age}. I live in $={country}.`
+let personInfoThree = `I am ${fullName} .I live in $={city}, ${country}. I am a ${job}.I teach ${language}.`
+ 
+console.log (personInfoOTwo);
+console.log (personInfoThree);
+
+function add (num1,num2){
+    return num1+num2
+} 
+ function main(){
+    const num = add (5,3) ;
+    console.log(num)
+ }
+ main()
+ function myFunction(num1,num2) {
+    return num1*num2
+ } 
+ function main () 
+    // const num  =add (10,5);
+// console.log(num)}
+
+ 
+ 
+//  function calcAge (Age) {
+   //  return Age*365 
+   //  console.log(Age) ;
+//  }
+  function deCode (code) {
+   if (code===1) {console.log('red')
+      
+   }
+  }
